41b6e4563bc69992d80b2a7ee7193f59922e0fd9
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-02-07 04:48:01 -0500

    eslint and working url popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState<string>("");
+  useEffect(() => {
+    const queryInfo = { active: true, lastFocusedWindow: true };
+    if (chrome.tabs) {
+      chrome.tabs.query(queryInfo, tabs => {
+        const url = tabs[0].url;
+        if (url) {
+          setUrl(url!);
+        }
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Current URL: {url}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
