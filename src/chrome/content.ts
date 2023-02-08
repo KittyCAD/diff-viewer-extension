02c13c642b6f46b9980fc8829a345e5c4fd0cc92
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-02-08 05:01:57 -0500

    comment and in-order multiple type support

--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -1,20 +1,18 @@
-const gitHubInjection = require("github-injection") // no ts support
+// https://github.com/OctoLinker/injection
+// maintained by octolinker, makes sure pages that are loaded through pjax are available for injection
+// no ts support
+const gitHubInjection = require("github-injection")
 
 const supportedFileTypes = [".obj"]
 
-function getElements(): HTMLElement[] {
-    const reference = document
-    const elements = []
-    for (const type of supportedFileTypes) {
-        const selector = `[data-file-type="${type}"] .js-file-content`
-        const nodes = [...reference.querySelectorAll(selector)]
-        elements.push(...nodes.map(n => n as HTMLElement))
-    }
-    return elements
+function getElements(document: Document): HTMLElement[] {
+    const fileTypeSelectors = supportedFileTypes.map(t => `[data-file-type="${t}"]`)
+    const selector = `:is(${fileTypeSelectors.join(",")}) .js-file-content`
+    return [...document.querySelectorAll(selector)].map(n => n as HTMLElement)
 }
 
 gitHubInjection(() => {
-    const elements = getElements()
+    const elements = getElements(window.document)
     console.log(`Found ${elements.length} elements`)
     for (const element of elements) {
         element.innerHTML = "kittycad material"
