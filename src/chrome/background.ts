b2f9b90ee7fbd13c36682a77b316abda7b9bc0a7
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-02-13 05:11:02 -0500

    working kittycad api in service worker

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -1,2 +1,20 @@
 // Will be needed for CORS to api.kittycad.io
-export {}
+import { Client, users } from "@kittycad/lib"
+import { User_type } from "@kittycad/lib/dist/types/src/models";
+import { Octokit } from "@octokit/rest";
+import { getStorageToken } from "./storage";
+
+let kittycad: Client;
+let octokit: Octokit;
+
+async function initAPIs() {
+    kittycad = new Client("c2703952-ad53-470e-a51a-71fafad1a16d");
+    const kittycadResponse = await users.get_user_self({ client: kittycad })
+    console.log(`Logged in on kittycad.io as ${(kittycadResponse as User_type).email}`)
+
+    octokit = new Octokit({ auth: await getStorageToken() })
+    const octokitResponse = await octokit.rest.users.getAuthenticated()
+    console.log(`Logged in on github.com as ${octokitResponse.data.login}`)
+}
+
+initAPIs()
