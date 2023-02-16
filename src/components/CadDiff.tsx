a922353f7b1821ad8f7b4cb74b0c26eeabc53195
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-02-16 07:09:58 -0500

    Working stl canvas

--- src/components/CadDiff.tsx
+++ src/components/CadDiff.tsx
@@ -1,9 +1,27 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader"
+import { BufferGeometry } from "three"
+import { Canvas } from "@react-three/fiber";
 
 function ModelView({ file }: { file: string }): React.ReactElement {
-    return <p>
-        {atob(file || "").slice(0, 200)}
-    </p>
+    const [geometry, setGeometry] = useState<BufferGeometry>()
+    useEffect(() => {
+        const loader = new STLLoader()
+        const geometry = loader.parse(atob(file))
+        console.log(`Model ${geometry.id} loaded`)
+        setGeometry(geometry)
+    }, [file])
+    return (
+        <Canvas>
+           <ambientLight />
+           <mesh geometry={geometry}>
+             <meshStandardMaterial color="lightgray" />
+           </mesh>
+            <OrbitControls />
+         </Canvas>
+    )
 }
 
 export type CadDiffProps = {
