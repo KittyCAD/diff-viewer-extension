7d5d1f20c52c0838a2820aee173c0e30eed24da5
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-02-17 06:03:45 -0500

    diff colors

--- src/components/CadDiff.tsx
+++ src/components/CadDiff.tsx
@@ -4,47 +4,49 @@ import { OrbitControls } from "@react-three/drei"
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader"
 import { BufferGeometry } from "three"
 import { Canvas } from "@react-three/fiber"
-import { Box, ThemeProvider } from "@primer/react"
+import { Box, ThemeProvider, useTheme } from "@primer/react"
 
 function ModelView({ file }: { file: string }): React.ReactElement {
-    const [geometry, setGeometry] = useState<BufferGeometry>()
-    useEffect(() => {
-        const loader = new STLLoader()
-        const geometry = loader.parse(atob(file))
-        console.log(`Model ${geometry.id} loaded`)
-        setGeometry(geometry)
-    }, [file])
-    return (
-        <Canvas>
-           <ambientLight />
-           <mesh geometry={geometry}>
-             <meshStandardMaterial color="lightgray" />
-           </mesh>
-            <OrbitControls />
-         </Canvas>
-    )
+  const { theme } = useTheme()
+  const [geometry, setGeometry] = useState<BufferGeometry>()
+  useEffect(() => {
+    const loader = new STLLoader()
+    const geometry = loader.parse(atob(file))
+    console.log(`Model ${geometry.id} loaded`)
+    setGeometry(geometry)
+  }, [file])
+  return (
+    <Canvas>
+      <ambientLight />
+      <mesh geometry={geometry}>
+        <meshStandardMaterial color={theme?.colors.fg.default} />
+      </mesh>
+      <OrbitControls />
+    </Canvas>
+  )
 }
 
 export type CadDiffProps = {
-    before?: string
-    after?: string
+  before?: string
+  after?: string
 }
 
 export function CadDiff({ before, after }: CadDiffProps): React.ReactElement {
-    return (
-        <ThemeProvider>
-            <Box display="flex">
-                <Box flexGrow={1}>
-                    {before &&
-                        <ModelView file={before} />
-                    }
-                </Box>
-                <Box flexGrow={1} borderLeftWidth={1} borderLeftColor="border.default" borderLeftStyle="solid">
-                    {after &&
-                        <ModelView file={after} />
-                    }
-                </Box>
-            </Box>
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider>
+      <Box display="flex" height={300}>
+        <Box flexGrow={1} backgroundColor="danger.subtle">
+          {before &&
+            <ModelView file={before} />
+          }
+        </Box>
+        <Box flexGrow={1} backgroundColor="success.subtle"
+          borderLeftWidth={1} borderLeftColor="border.default" borderLeftStyle="solid">
+          {after &&
+            <ModelView file={after} />
+          }
+        </Box>
+      </Box>
+    </ThemeProvider>
+  )
 }
