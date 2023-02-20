97c88d159649e1c438e09c445d163caa82f6cee2
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-02-20 07:54:36 -0500

    Working test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,10 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  // Waiting for loading
+  setTimeout(() => {
+    const element = screen.getByText(/github token/i);
+    expect(element).toBeInTheDocument();
+  }, 2000);
 });
