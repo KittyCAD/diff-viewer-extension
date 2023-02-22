b3a51531f3fc506b8cb468a81c8962fe6e489178
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-02-22 05:05:27 -0500

    working caddiff test

---
+++ jest.mockundefined.js
@@ -0,0 +1,2 @@
+// From https://stackoverflow.com/a/71088205, this allows to not test the untransformed files themselves
+module.exports = undefined
---
+++ src/components/CadDiff.test.tsx
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import { CadDiff } from "./CadDiff";
+
+it("renders the CAD diff element", () => {
+  render(<CadDiff />);
+});
