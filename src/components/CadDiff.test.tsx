1beef8cc59c7d86fc04df386beb8710c9fe0bb87
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-02-22 05:26:49 -0500

    Note

--- src/components/CadDiff.test.tsx
+++ src/components/CadDiff.test.tsx
@@ -3,4 +3,8 @@ import { CadDiff } from "./CadDiff";
 
 it("renders the CAD diff element", () => {
   render(<CadDiff />);
+
+  // TODO: find a way to add proper tests for ModelView,
+  // seems non-trivial with the simulated DOM
+  // Probably will have to go for end-to-end
 });
