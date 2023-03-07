3f6c1043cb5572c43a34cae74e76b199a4c772de
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-03-07 07:14:37 -0500

    First working test, clean up

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from './fixtures'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+test('popup page', async ({ page, extensionId }) => {
+    await page.goto(`chrome-extension://${extensionId}/index.html`)
+    await expect(page.locator('body')).toContainText('Enter a GitHub token')
+})
