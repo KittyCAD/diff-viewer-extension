db72313bacc4812ee14524585fc9a807ab001d49
Pierre Jacquier <pierrejacquier39@gmail.com>
2023-07-04 18:40:03 -0400

    Listen for Return in Settings page Fixes #279

--- src/components/settings/TokenForm.tsx
+++ src/components/settings/TokenForm.tsx
@@ -3,11 +3,13 @@ import { useState, PropsWithChildren } from 'react'
 
 export type TokenFormProps = {
     service: string
+    loading: boolean
     onToken: (token: string) => void
 }
 
 export function TokenForm({
     service,
+    loading,
     onToken,
     children,
 }: PropsWithChildren<TokenFormProps>) {
@@ -20,7 +22,9 @@ export function TokenForm({
                 <TextInput
                     alt="Text input for token"
                     value={token}
+                    loading={loading}
                     onChange={e => setToken(e.target.value)}
+                    onKeyDown={e => e.key === 'Enter' && onToken(token)}
                 />
                 {children}
             </FormControl>
