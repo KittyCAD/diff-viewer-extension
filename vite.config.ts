1afe37884935d876e98ed50b788053b89bc9b37a
Pierre Jacquier <pierrejacquier39@gmail.com>
2024-01-21 20:31:15 -0500

    Migration to vite v5 (#536)
    * Migration to vite v5
    Fixes #511
    
    * Lint

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,29 @@ import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 
+const viteManifestHackIssue846: Plugin & {
+    renderCrxManifest: (manifest: any, bundle: any) => void
+} = {
+    // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
+    name: 'manifestHackIssue846',
+    renderCrxManifest(_manifest, bundle) {
+        bundle['manifest.json'] = bundle['.vite/manifest.json']
+        bundle['manifest.json'].fileName = 'manifest.json'
+        delete bundle['.vite/manifest.json']
+    },
+}
+
 export default defineConfig(() => {
     return {
         build: {
             outDir: 'build',
         },
-        plugins: [react(), crx({ manifest }), nodePolyfills()],
+        plugins: [
+            react(),
+            viteManifestHackIssue846,
+            crx({ manifest }),
+            nodePolyfills(),
+        ],
         resolve: {
             alias: {
                 // Replaces node-fetch in kittycad.ts, cross-fetch wouldn't work
