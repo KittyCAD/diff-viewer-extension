b7afdb2a5f5eab95cb7d297306deb9647617b2de
Pierre Jacquier <pierrejacquier39@gmail.com>
2025-01-03 15:14:44 -0500

    Fix content script on Chrome v130+ (#642)
    * Bump to @crxjs/vite-plugin@2.0.0-beta.26
    
    * Clean up old workaround
    
    * Pin runner to 20.04
    
    * Add fixme on a blob preview test

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,6 @@ import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 
-const viteManifestHackIssue846: Plugin & {
-    renderCrxManifest: (manifest: any, bundle: any) => void
-} = {
-    // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
-    name: 'manifestHackIssue846',
-    renderCrxManifest(_manifest, bundle) {
-        bundle['manifest.json'] = bundle['.vite/manifest.json']
-        bundle['manifest.json'].fileName = 'manifest.json'
-        delete bundle['.vite/manifest.json']
-    },
-}
-
 export default defineConfig(() => {
     return {
         build: {
@@ -24,7 +12,6 @@ export default defineConfig(() => {
         },
         plugins: [
             react(),
-            viteManifestHackIssue846,
             crx({ manifest }),
             nodePolyfills(),
         ],
